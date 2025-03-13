de22fb1744b1f3f67c9f0fb7a56d3a09d6d51f4a
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-13 17:24:01 +0800

    subtraction of two numbers.py

--- subtraction of two numbers.py
+++ subtraction of two numbers.py
@@ -1,4 +1,10 @@
-# subtraction of two numbers 
 # ask the user to input two numbers
+print("please enter two numbers")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # proceed with the operation
+difference = num1 - num2
+
 # show the results
+print("the difference of the two numbers is" , difference)
